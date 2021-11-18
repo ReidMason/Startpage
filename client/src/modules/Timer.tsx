7477c0a68a48701aa9180e7a09b48ee1c6b38e30
Reid Mason <maddogshain132@gmail.com>
2021-11-18 11:35:19 +0000

    Finished adding functionality for alarm

--- client/src/modules/Timer.tsx
+++ client/src/modules/Timer.tsx
@@ -15,7 +15,38 @@ const zeroPadNum = (text: string | number) => {
     return String(text).padStart(2, '0')
 }
 
+const createAudioObject = () => {
+    const newAudio = new Audio("/android_argon.mp3");
+    newAudio.loop = true;
+    return newAudio;
+}
+
+const playAlarm = () => {
+    audio = createAudioObject();
+    audio.play();
+}
+
+const stopAlarm = () => {
+    audio.pause();
+}
+
+const timerToSeconds = (timer: Array<TimerDigit>): number => {
+    const hours = (timer[0].value * 10) + timer[1].value;
+    const minutes = (timer[2].value * 10) + timer[3].value;
+    const seconds = (timer[4].value * 10) + timer[5].value;
+
+    return (hours * 3600) + (minutes * 60) + seconds;
+}
+
+const secondsToTimerString = (seconds: number): string => {
+    const hoursRemaining = Math.floor(seconds / 3600)
+    const minutesRemaining = Math.floor((seconds - (hoursRemaining * 3600)) / 60);
+    const secondsRemaining = seconds - (hoursRemaining * 3600) - (minutesRemaining * 60);
+    return `${zeroPadNum(hoursRemaining)}${zeroPadNum(minutesRemaining)}${zeroPadNum(secondsRemaining)}`;
+}
+
 let interval: NodeJS.Timeout | null = null;
+let audio = createAudioObject();
 
 export default function Timer() {
     const [timer, setTimer] = useState<Array<TimerDigit>>(padArray([], 6, { known: false, value: 0 }));
@@ -23,6 +54,7 @@ export default function Timer() {
     const [timerRunning, setTimerRunning] = useState<boolean>(false);
     const [editMode, setEditMode] = useState<boolean>(true);
     const [editModeFirstRender, setEditModeFirstRender] = useState<boolean>(true);
+    const [timerFinished, setTimerFinished] = useState<boolean>(false);
 
     const anyTimerValueEntered = timer.find(x => x.known);
     const timeDisplay = timer.filter(x => x.known).map(x => x.value).join("");
@@ -45,34 +77,30 @@ export default function Timer() {
 
     const startTimer = () => {
         setEditMode(false);
-        const hours = (timeRemaining[0].value * 10) + timeRemaining[1].value;
-        const minutes = (timeRemaining[2].value * 10) + timeRemaining[3].value;
-        const seconds = (timeRemaining[4].value * 10) + timeRemaining[5].value;
-
-        var totalTime = (hours * 3600) + (minutes * 60) + seconds;
+        let totalTime = timerToSeconds(timeRemaining);
         if (totalTime < 1)
             return;
 
         setTimerRunning(true);
 
+        // Clear existing interval to prevent multiple running
         if (interval)
             clearInterval(interval);
 
-
+        // Alarm countdown
         interval = setInterval(() => {
             totalTime--;
-            const hoursRemaining = Math.floor(totalTime / 3600)
-            const minutesRemaining = Math.floor((totalTime - (hoursRemaining * 3600)) / 60);
-            const secondsRemaining = totalTime - (hoursRemaining * 3600) - (minutesRemaining * 60);
-            const timeRemainingDisplay = `${zeroPadNum(hoursRemaining)}${zeroPadNum(minutesRemaining)}${zeroPadNum(secondsRemaining)}`;
 
+            const timeRemainingDisplay = secondsToTimerString(totalTime);
             const inputValueArray = timeRemainingDisplay.split('').map((x, i) => ({ known: timeRemaining[i].known, value: parseInt(x) }));
             const newTimeRemainingDisplay = padArray(inputValueArray, 6, { known: false, value: 0 })
-
             setTimeRemaining([...newTimeRemainingDisplay])
 
-            if (totalTime <= 0 && interval)
+            if (totalTime <= 0 && interval) {
+                setTimerFinished(true);
+                playAlarm();
                 stopTimer();
+            }
         }, 1000);
     }
 
@@ -108,26 +136,42 @@ export default function Timer() {
     }
 
     const enableEditMode = () => {
-        if (timerRunning || timerAndTimeRemainingMatch)
+        // Don't allow editing if the timer is running
+        if (timerRunning)
+            return;
+
+        // If the timer has ended and we're entering edit mode we can reset the timer
+        if (timerToSeconds(timeRemaining) < 1)
             resetTimer();
+
         setEditMode(true);
+        acknowledgeAlarmFinished()
         setEditModeFirstRender(false);
+    }
+
+    const acknowledgeAlarmFinished = () => {
+        stopAlarm();
+        setTimerFinished(false);
     }
 
     return (
         <div className="flex flex-col gap-4 items-center">
             <div className="flex flex-col items-center gap-4">
-                <label className={`${!editMode || editModeFirstRender ? "cursor-pointer" : ""}`} onClick={enableEditMode} onMouseDown={(e) => e.preventDefault()} >
+                <label className={`pl-4 ${timerFinished ? "animate-bounce" : ""} ${(!editMode || editModeFirstRender) && !timerRunning ? "cursor-pointer" : ""}`} onClick={enableEditMode} onMouseDown={(e) => e.preventDefault()} >
                     <TimerDisplay editMode={editMode} editModeFirstRender={editModeFirstRender} timer={editMode ? timer : timeRemaining} />
                     <input className="text-black opacity-0 absolute w-0 h-0" autoComplete="off" id="timeEditor" onBlur={() => setEditMode(false)} onSubmit={startTimer} value={timeDisplay} onChange={handleTimerInput} type="tel" maxLength={6} />
                 </label>
 
-                <div className="flex gap-4">
+                <div className="grid grid-cols-2 gap-4">
                     {!timerRunning ?
-                        <Button onClick={startTimer} disabled={!anyTimerValueEntered}>Start</Button>
+                        timerFinished ?
+                            <Button className="w-64" onClick={acknowledgeAlarmFinished} disabled={!anyTimerValueEntered}>Okay</Button>
+                            :
+                            <Button className="w-64" onClick={startTimer} disabled={!anyTimerValueEntered}>Start</Button>
                         :
-                        <Button onClick={stopTimer}>Stop</Button>
+                        <Button className="w-64" onClick={stopTimer}>Stop</Button>
                     }
+
                     <Button onClick={resetTimer} disabled={!anyTimerValueEntered || timerAndTimeRemainingMatch}>Reset</Button>
                 </div>
             </div>
