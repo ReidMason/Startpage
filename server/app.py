aaa04d76705fc6fde76d2744c9056235c58a6ae9
Reid Mason <maddogshain132@gmail.com>
2021-11-21 12:26:56 +0000

    Added rain chance to detailed weather display

--- server/app.py
+++ server/app.py
@@ -1,17 +1,21 @@
 from flask import Flask, jsonify, request, send_file, render_template
 from flask_cors import CORS
 
+from models.weather import Weather
 from utils import serialize
 from services.weatherService import get_weather
-from caching import run_function
+from caching import run_function, clear_cache
 from services.configService import load_config, save_config
 
 app = Flask(__name__)
 CORS(app)
+clear_cache()
+
 
 @app.route("/")
 def index():
     return send_file('../client/build/index.html')
+
 
 @app.route('/static/js/<path>')
 def data(path: str):
@@ -21,6 +25,7 @@ def data(path: str):
 @app.route('/static/css/<path>')
 def css(path: str):
     return send_file(f'../client/build/static/css/{path}')
+
 
 @app.route("/api/config", methods = ["GET", "PUT"])
 def get_config():
@@ -36,17 +41,8 @@ def get_weather_info():
     weather_config = load_config().weather
     weather_response = run_function(get_weather, weather_config.location)
 
-    if weather_response.get('cod') != 200:
-        return jsonify(None)
+    return jsonify(serialize(Weather(weather_response)))
 
-    weather = weather_response.get('weather')[0]
-    data = {'temperature': weather_response.get('main').get('temp'),
-            'icon'       : weather.get('icon'),
-            'description': weather.get('description'),
-            'feelsLike': weather_response.get('main').get('feels_like')
-            }
-
-    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5003)
