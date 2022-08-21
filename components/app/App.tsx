def5f48962a3408fe9508b8eb11b4778c26dc2ef
Reid Mason <maddogshain132@gmail.com>
2022-08-21 22:12:13 +0100

    Truncated app url

--- components/app/App.tsx
+++ components/app/App.tsx
@@ -11,23 +11,21 @@ export default function App({ app, preview }: AppProps) {
   const displayUrl = app.url.replace(/^https?:\/\//gi, "").split("/", 1)[0];
 
   return (
-    <div className="overflow-hidden rounded-lg dark:text-primary-100">
-      <a
-        className={`flex flex-1 items-center overflow-hidden text-ellipsis  p-2 pl-4 duration-200 first-line:transition hover:scale-105 hover:bg-primary-100 hover:shadow-lg focus:bg-primary-100 focus:shadow-lg focus:outline-none hover:dark:bg-primary-100/30 focus:dark:bg-primary-100/30`}
-        {...(preview ? {} : { href: app.url })}
-      >
-        <div className="mr-2 text-4xl">
-          <Icon icon={app.icon || "entypo:new-message"} />
-        </div>
-        <div>
-          <p className="lowercase dark:text-primary-50">
-            {app.name || "New app"}
-          </p>
-          <p className="text-xs text-primary-300 dark:text-white">
-            {displayUrl || "app.example.com"}
-          </p>
-        </div>
-      </a>
-    </div>
+    <a
+      className="flex rounded-lg p-2 duration-200 first-line:transition hover:scale-105 hover:bg-primary-100 hover:shadow-lg focus:bg-primary-100 focus:shadow-lg focus:outline-none dark:text-primary-100 hover:dark:bg-primary-100/30 focus:dark:bg-primary-100/30"
+      {...(preview ? {} : { href: app.url })}
+    >
+      <div className="mr-2 text-4xl">
+        <Icon icon={app.icon || "entypo:new-message"} />
+      </div>
+      <div className="overflow-hidden">
+        <p className="lowercase dark:text-primary-50">
+          {app.name || "New app"}
+        </p>
+        <p className="truncate text-xs text-primary-300 dark:text-white">
+          {displayUrl || "app.example.com"}
+        </p>
+      </div>
+    </a>
   );
 }
