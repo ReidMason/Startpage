b3147e2900835060bf69a27b890b0ebc2ca8fc5a
Reid Mason <maddogshain132@gmail.com>
2022-09-02 13:23:13 +0100

    Cleaned up debug logs

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -4,7 +4,6 @@ export default async function revalidate(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("Revalidating");
   await res.revalidate("/");
   return res.status(200).send("Revalidated");
 }
