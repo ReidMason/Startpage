1829a85d296d1e82e4bde7b1f62f018af48f69a7
Reid Mason <maddogshain132@gmail.com>
2022-09-03 20:17:54 +0100

    Removed headers

--- next.config.js
+++ next.config.js
@@ -6,20 +6,6 @@ const nextConfig = {
   images: {
     domains: ["openweathermap.org"],
   },
-  async headers() {
-    return [
-      {
-        source: "/:all*(svg|jpg|png)",
-        locale: false,
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, stale-while-revalidate",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
