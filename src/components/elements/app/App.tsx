3f6be132d0e502636e3a2ee892e8e5e43a1d386a
Reid Mason <maddogshain132@gmail.com>
2022-09-03 01:00:40 +0100

    Fixed hover styling for apps

--- src/components/elements/app/App.tsx
+++ src/components/elements/app/App.tsx
@@ -13,21 +13,23 @@ export default function App({ app, preview }: AppProps) {
   const nonPreviewStyling = preview ? "" : "hover:scale-105";
 
   return (
-    <a
-      className={`${nonPreviewStyling} flex rounded-lg p-2 duration-200 first-line:transition hover:bg-primary-100 hover:shadow-lg focus:bg-primary-100 focus:shadow-lg focus:outline-none dark:text-primary-100 hover:dark:bg-primary-100/30 focus:dark:bg-primary-100/30`}
-      {...(preview ? {} : { href: app.url })}
-    >
-      <div className="mr-2 text-4xl">
-        <Icon icon={app.icon || "entypo:new-message"} />
-      </div>
-      <div className="overflow-hidden">
-        <p className="lowercase dark:text-primary-50">
-          {app.name || "New app"}
-        </p>
-        <p className="truncate text-xs text-primary-300 dark:text-white">
-          {displayUrl || "app.example.com"}
-        </p>
-      </div>
-    </a>
+    <div className={`${nonPreviewStyling} transition duration-200`}>
+      <a
+        className="flex rounded-lg p-2 transition duration-200 hover:bg-primary-100 hover:shadow-lg focus:bg-primary-100 focus:shadow-lg focus:outline-none dark:text-primary-100 hover:dark:bg-primary-100/30 focus:dark:bg-primary-100/30"
+        {...(preview ? {} : { href: app.url })}
+      >
+        <div className="mr-2 text-4xl">
+          <Icon icon={app.icon || "entypo:new-message"} />
+        </div>
+        <div className="overflow-hidden">
+          <p className="lowercase dark:text-primary-50">
+            {app.name || "New app"}
+          </p>
+          <p className="truncate text-xs text-primary-300 dark:text-white">
+            {displayUrl || "app.example.com"}
+          </p>
+        </div>
+      </a>
+    </div>
   );
 }
