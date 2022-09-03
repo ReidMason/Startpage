8547e5d41af3b8ab86c3dc1be9cfae763df9bc9a
Reid Mason <maddogshain132@gmail.com>
2022-09-03 18:59:42 +0100

    Made revalidate call relative

--- src/hooks/useConfig.ts
+++ src/hooks/useConfig.ts
@@ -2,7 +2,7 @@ import { Config } from "../backend/routers/config/schemas";
 import { trpc } from "../utils/trpc";
 
 async function revalidate() {
-  await fetch("http://localhost:3000/api/revalidate");
+  await fetch("/api/revalidate");
 }
 
 export default function useConfig(onSuccess?: (config: Config) => void) {
