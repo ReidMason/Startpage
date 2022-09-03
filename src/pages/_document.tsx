cd2e47c5ba98364448d04fb10a3cc173c2811b5f
Reid Mason <maddogshain132@gmail.com>
2022-09-03 21:20:47 +0100

    Fixed glassy class assignment

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,19 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { getGlobalClasses } from "../../utils";
+import { getDocumentClasses, getBodyClasses } from "../../utils";
 
 export default class MyDocument extends Document {
   render() {
     const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
 
     return (
-      <Html lang="en" className={getGlobalClasses(pageProps.config)}>
+      <Html lang="en" className={getDocumentClasses(pageProps.config)}>
         <Head />
         <meta name="description" content="Startpage" />
-        <body className="bg-primary-50/90 dark:bg-primary-900/90">
+        <body
+          className={`bg-primary-50/90 dark:bg-primary-900/90 ${getBodyClasses(
+            pageProps.config
+          )}`}
+        >
           <Main />
           <NextScript />
         </body>
