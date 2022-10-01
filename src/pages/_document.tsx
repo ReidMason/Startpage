c8b69b14e3170af0c415d39aa46b19a058cc277c
Reid Mason <maddogshain132@gmail.com>
2022-10-01 13:36:50 +0100

    Switched document back to functional component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,23 +1,22 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+// import Document, { Html, Head, Main, NextScript } from "next/document";
+import { DocumentProps, Html, Main, NextScript, Head } from "next/document";
 import { getDocumentClasses, getBodyClasses } from "../../utils";
 
-export default class MyDocument extends Document {
-  render() {
-    const pageProps = this.props?.__NEXT_DATA__?.props?.pageProps;
+export default function Document({ __NEXT_DATA__ }: DocumentProps) {
+  const pageProps = __NEXT_DATA__.props.pageProps;
 
-    return (
-      <Html lang="en" className={getDocumentClasses(pageProps.config)}>
-        <Head />
-        <meta name="description" content="Startpage" />
-        <body
-          className={`bg-primary-50/90 dark:bg-primary-900/90 ${getBodyClasses(
-            pageProps.config
-          )}`}
-        >
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+  return (
+    <Html lang="en" className={getDocumentClasses(pageProps.config)}>
+      <Head />
+      <meta name="description" content="Startpage" />
+      <body
+        className={`bg-primary-50/90 dark:bg-primary-900/90 ${getBodyClasses(
+          pageProps.config
+        )}`}
+      >
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
