e53cbbe796af33e32a3bd955d301fc245a8d513a
Reid Mason <maddogshain132@gmail.com>
2023-11-15 22:12:29 +0000

    Started rewrite

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,5 @@
 import { postRouter } from "~/server/api/routers/post";
+import { configRouter } from "~/server/api/routers/config";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   post: postRouter,
+  config: configRouter,
 });
 
 // export type definition of API
