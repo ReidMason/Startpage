7d93e83aa6142ecad2aea8187f08533d298bd8e3
Reid Mason <maddogshain132@gmail.com>
2023-11-16 01:17:37 +0000

    Added basic config loading

---
+++ src/services/config/config.ts
@@ -0,0 +1,29 @@
+"use server";
+
+import { Config, configSchema } from "./schemas";
+import fs from "fs";
+
+const CONFIG_PATH = `${process.cwd()}/data/config.json`;
+const defaultConfig = configSchema.parse({});
+
+export async function getConfig(): Promise<Config> {
+  ensureConfigExists();
+  // TODO: Parse with zod
+  const config: Config = JSON.parse(fs.readFileSync(CONFIG_PATH, "utf8"));
+
+  return config;
+}
+
+function ensureConfigExists() {
+  // Try getting stats for the cache file, if it errors the file doesn't exist so we need to create it
+  try {
+    fs.statSync(CONFIG_PATH);
+  } catch (ex) {
+    console.warn("Failed to find config file. Generating a new one.");
+    saveConfig(defaultConfig);
+  }
+}
+
+function saveConfig(newConfig: Config) {
+  fs.writeFileSync(CONFIG_PATH, JSON.stringify(newConfig));
+}
