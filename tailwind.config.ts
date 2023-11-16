8b119a766affa9f90e6d828b40725c613e9f149e
Reid Mason <maddogshain132@gmail.com>
2023-11-16 01:33:47 +0000

    Added tailwind variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,82 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: {
+          50: "rgb(var(--colour-primary-50) / <alpha-value>)",
+          100: "rgb(var(--colour-primary-100) / <alpha-value>)",
+          200: "rgb(var(--colour-primary-200) / <alpha-value>)",
+          300: "rgb(var(--colour-primary-300) / <alpha-value>)",
+          400: "rgb(var(--colour-primary-400) / <alpha-value>)",
+          500: "rgb(var(--colour-primary-500) / <alpha-value>)",
+          600: "rgb(var(--colour-primary-600) / <alpha-value>)",
+          700: "rgb(var(--colour-primary-700) / <alpha-value>)",
+          800: "rgb(var(--colour-primary-800) / <alpha-value>)",
+          900: "rgb(var(--colour-primary-900) / <alpha-value>)",
+        },
+        green: {
+          50: "rgb(var(--colour-green-50) / <alpha-value>)",
+          100: "rgb(var(--colour-green-100) / <alpha-value>)",
+          200: "rgb(var(--colour-green-200) / <alpha-value>)",
+          300: "rgb(var(--colour-green-300) / <alpha-value>)",
+          400: "rgb(var(--colour-green-400) / <alpha-value>)",
+          500: "rgb(var(--colour-green-500) / <alpha-value>)",
+          600: "rgb(var(--colour-green-600) / <alpha-value>)",
+          700: "rgb(var(--colour-green-700) / <alpha-value>)",
+          800: "rgb(var(--colour-green-800) / <alpha-value>)",
+          900: "rgb(var(--colour-green-900) / <alpha-value>)",
+        },
+        yellow: {
+          50: "rgb(var(--colour-yellow-50) / <alpha-value>)",
+          100: "rgb(var(--colour-yellow-100) / <alpha-value>)",
+          200: "rgb(var(--colour-yellow-200) / <alpha-value>)",
+          300: "rgb(var(--colour-yellow-300) / <alpha-value>)",
+          400: "rgb(var(--colour-yellow-400) / <alpha-value>)",
+          500: "rgb(var(--colour-yellow-500) / <alpha-value>)",
+          600: "rgb(var(--colour-yellow-600) / <alpha-value>)",
+          700: "rgb(var(--colour-yellow-700) / <alpha-value>)",
+          800: "rgb(var(--colour-yellow-800) / <alpha-value>)",
+          900: "rgb(var(--colour-yellow-900) / <alpha-value>)",
+        },
+        grey: {
+          50: "rgb(var(--colour-grey-50) / <alpha-value>)",
+          100: "rgb(var(--colour-grey-100) / <alpha-value>)",
+          200: "rgb(var(--colour-grey-200) / <alpha-value>)",
+          300: "rgb(var(--colour-grey-300) / <alpha-value>)",
+          400: "rgb(var(--colour-grey-400) / <alpha-value>)",
+          500: "rgb(var(--colour-grey-500) / <alpha-value>)",
+          600: "rgb(var(--colour-grey-600) / <alpha-value>)",
+          700: "rgb(var(--colour-grey-700) / <alpha-value>)",
+          800: "rgb(var(--colour-grey-800) / <alpha-value>)",
+          900: "rgb(var(--colour-grey-900) / <alpha-value>)",
+        },
+        red: {
+          50: "rgb(var(--colour-red-50) / <alpha-value>)",
+          100: "rgb(var(--colour-red-100) / <alpha-value>)",
+          200: "rgb(var(--colour-red-200) / <alpha-value>)",
+          300: "rgb(var(--colour-red-300) / <alpha-value>)",
+          400: "rgb(var(--colour-red-400) / <alpha-value>)",
+          500: "rgb(var(--colour-red-500) / <alpha-value>)",
+          600: "rgb(var(--colour-red-600) / <alpha-value>)",
+          700: "rgb(var(--colour-red-700) / <alpha-value>)",
+          800: "rgb(var(--colour-red-800) / <alpha-value>)",
+          900: "rgb(var(--colour-red-900) / <alpha-value>)",
+        },
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    plugin(function({ addVariant }) {
+      addVariant("glassy", ".glassy &");
+    }),
+  ],
+};
+export default config;
