bef86a65debfcf78665c565b64f131ac704ee840
Reid Mason <maddogshain132@gmail.com>
2023-11-18 22:56:35 +0000

    Added searchbar

--- src/components/MainDisplay/MainDisplay.tsx
+++ src/components/MainDisplay/MainDisplay.tsx
@@ -3,6 +3,7 @@
 import { Config } from "@/services/config/schemas";
 import React, { useState } from "react";
 import AppsGrid from "../Apps/AppsGrid";
+import Searchbar from "../Searchbar/Searchbar";
 
 interface MainDisplayProps {
   config: Config;
@@ -13,6 +14,7 @@ export default function MainDisplay({ config }: MainDisplayProps) {
 
   return (
     <div className="container mx-auto mb-20 gap-8 p-8 text-primary-300 transition bg-primary-transparent-900 glassy:backdrop-blur-xl dark:text-primary-100 sm:p-16 sm:glassy:rounded-2xl">
+      <Searchbar config={config} setAppFilter={setFilter} />
       <AppsGrid config={config} appFilter={filter} />
     </div>
   );
