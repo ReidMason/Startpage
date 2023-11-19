63905b9e353bbdf76dc08fcd025d1fb83857ca72
Reid Mason <maddogshain132@gmail.com>
2023-11-19 00:05:20 +0000

    Added greeting display

--- src/components/MainDisplay/MainDisplay.tsx
+++ src/components/MainDisplay/MainDisplay.tsx
@@ -4,6 +4,7 @@ import { Config } from "@/services/config/schemas";
 import React, { useState } from "react";
 import AppsGrid from "../Apps/AppsGrid";
 import Searchbar from "../Searchbar/Searchbar";
+import Greeting from "../Greeting/Greeting";
 
 interface MainDisplayProps {
   config: Config;
@@ -13,8 +14,11 @@ export default function MainDisplay({ config }: MainDisplayProps) {
   const [filter, setFilter] = useState("");
 
   return (
-    <div className="container mx-auto mb-20 gap-8 p-8 text-primary-300 transition bg-primary-transparent-900 glassy:backdrop-blur-xl dark:text-primary-100 sm:p-16 sm:glassy:rounded-2xl">
+    <div className="container mx-auto mb-20 flex flex-col gap-8 p-8 text-primary-300 transition bg-primary-transparent-900 glassy:backdrop-blur-xl dark:text-primary-100 sm:p-16 sm:glassy:rounded-2xl">
       <Searchbar config={config} setAppFilter={setFilter} />
+      <div className="mb-4">
+        <Greeting config={config} />
+      </div>
       <AppsGrid config={config} appFilter={filter} />
     </div>
   );
