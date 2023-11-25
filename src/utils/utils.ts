07be8d4d853c148f622c53df3a28c370c5ced5a3
Reid Mason <maddogshain132@gmail.com>
2023-11-25 16:38:03 +0000

    Added "new app" button

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,12 @@
+import { v4 as uuidv4 } from "uuid";
+
 export function getUnixTime(): number {
   // Gets unix time in seconds
   return Math.floor(Date.now() / 1000);
+}
+
+export function generateUuid(): string {
+  return uuidv4();
 }
 
 export function isValidUrl(text: string) {
