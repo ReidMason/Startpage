bd0d1fe177834d57fe864e244d88f742f31b5092
Reid Mason <maddogshain132@gmail.com>
2023-11-29 00:30:02 +0000

    Tidied up dom elements for home tab

--- src/components/SettingsSidebar/Home.tsx
+++ src/components/SettingsSidebar/Home.tsx
@@ -7,26 +7,22 @@ export const Home = ({ setActivePage }: SettingsSectionProps) => {
 
   return (
     <>
-      <div className="flex-col py-4 shrink-0 items-center">
-        <h2 className="font-semibold text-white text-xl">Settings</h2>
-      </div>
-      <div className="flex flex-1 flex-col">
-        <ul role="list" className="flex flex-1 flex-col gap-1">
-          {navigation.map((item) => (
-            <li key={item.name}>
-              <button
-                className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full"
-                onClick={() => setActivePage(item.page)}
-              >
-                {item.icon && (
-                  <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
-                )}
-                {item.name}
-              </button>
-            </li>
-          ))}
-        </ul>
-      </div>
+      <h2 className="font-semibold text-white py-4 text-xl">Settings</h2>
+      <ul role="list" className="flex flex-1 flex-col gap-1">
+        {navigation.map((item) => (
+          <li key={item.name}>
+            <button
+              className="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold w-full"
+              onClick={() => setActivePage(item.page)}
+            >
+              {item.icon && (
+                <item.icon className="h-6 w-6 shrink-0" aria-hidden="true" />
+              )}
+              {item.name}
+            </button>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
