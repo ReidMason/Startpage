c35d559e6ea75e5583dcaf894c8b08a362f44569
Reid Mason <maddogshain132@gmail.com>
2023-11-30 18:35:25 +0000

    Started improving settings styling

--- src/components/SettingsSidebar/General.tsx
+++ src/components/SettingsSidebar/General.tsx
@@ -3,16 +3,19 @@ import { SettingsSectionProps } from "./types";
 import SettingsPanelWrapper from "./SettingsPanelWrapper";
 import Input from "../Input/Input";
 import Label from "../Label/Label";
+import SettingsGroup from "../Layouts/SettingsGroup";
 
 export const General = ({ ...props }: SettingsSectionProps) => {
   return (
     <SettingsPanelWrapper {...props}>
-      <Label text="Search placeholder" htmlFor="SearchPlaceholder">
-        <Input
-          register={props.register("general.searchPlaceholder")}
-          id="SearchPlaceholder"
-        />
-      </Label>
+      <SettingsGroup>
+        <Label text="Search placeholder" htmlFor="SearchPlaceholder">
+          <Input
+            register={props.register("general.searchPlaceholder")}
+            id="SearchPlaceholder"
+          />
+        </Label>
+      </SettingsGroup>
     </SettingsPanelWrapper>
   );
 };
--- src/components/SettingsSidebar/Weather.tsx
+++ src/components/SettingsSidebar/Weather.tsx
@@ -5,25 +5,59 @@ import { Toggle } from "../Toggle/Toggle";
 import SettingsPanelWrapper from "./SettingsPanelWrapper";
 import Label from "../Label/Label";
 import Input from "../Input/Input";
+import SettingsGroup from "../Layouts/SettingsGroup";
 
 export default function Weather({ ...props }: SettingsSectionProps) {
   return (
     <SettingsPanelWrapper {...props}>
-      <Label text="Weather enabled" htmlFor="WeatherEnabled">
-        <Controller
-          name="weather.enabled"
-          control={props.control}
-          render={({ field: { ref, ...field } }) => (
-            <Toggle {...field} id="WeatherEnabled" />
-          )}
-        ></Controller>
+      <SettingsGroup>
+        <Label text="Weather enabled" htmlFor="WeatherEnabled">
+          <Controller
+            name="weather.enabled"
+            control={props.control}
+            render={({ field: { ref, ...field } }) => (
+              <Toggle {...field} id="WeatherEnabled" />
+            )}
+          />
+        </Label>
+
         <Label text="ApiKey" htmlFor="WeatherApiKey">
           <Input
             register={props.register("weather.apiKey")}
             id="WeatherApiKey"
+            placeholder="Open weather api key"
           />
         </Label>
-      </Label>
+      </SettingsGroup>
+
+      <SettingsGroup>
+        <Label text="Location" htmlFor="WeatherLocation">
+          <Input
+            register={props.register("weather.location")}
+            id="WeatherLocation"
+            placeholder="Weather location"
+          />
+        </Label>
+
+        <Label text="Detailed" htmlFor="WeatherDetailed">
+          <Controller
+            name="weather.detailed"
+            control={props.control}
+            render={({ field: { ref, ...field } }) => (
+              <Toggle {...field} id="WeatherDetailed" />
+            )}
+          />
+        </Label>
+        <Label text="Show location" htmlFor="WeatherShowLocation">
+          <Controller
+            name="weather.showLocation"
+            control={props.control}
+            render={({ field: { ref, ...field } }) => (
+              <Toggle {...field} id="WeatherShowLocation" />
+            )}
+          />
+        </Label>
+      </SettingsGroup>
     </SettingsPanelWrapper>
   );
 }
