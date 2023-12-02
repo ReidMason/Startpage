c1a7da31f4427a3885c4d74678a57c4719ea5196
Reid Mason <maddogshain132@gmail.com>
2023-12-02 20:06:33 +0000

    Improved styling for settings section

--- src/components/Layouts/SettingsGroup.tsx
+++ src/components/Layouts/SettingsGroup.tsx
@@ -7,9 +7,12 @@ export default function SettingsGroup({
   const childrenArray = Children.toArray(children);
 
   return (
-    <div {...props} className="flex flex-col rounded-lg dark:bg-gray-800">
+    <div
+      {...props}
+      className="flex flex-col rounded-lg dark:bg-gray-800 gap-2 py-2"
+    >
       {childrenArray.map((child, i) => (
-        <div key={i} className="flex flex-col gap-2 pb-2">
+        <div key={i} className="flex flex-col gap-2">
           <div className="px-2">{child}</div>
           {i < childrenArray.length - 1 && <hr className="opacity-10" />}
         </div>
