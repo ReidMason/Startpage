5c1ddba0511d21da7356e985122cb795d030fb56
Reid Mason <maddogshain132@gmail.com>
2023-12-03 00:16:52 +0000

    Adjusted colours for inputs

--- src/components/Label/Label.tsx
+++ src/components/Label/Label.tsx
@@ -1,3 +1,4 @@
+import { classNames } from "@/utils/utils";
 import React, { HTMLAttributes } from "react";
 
 interface LabelProps extends HTMLAttributes<HTMLLabelElement> {
@@ -10,7 +11,10 @@ export default function Label({ children, text, ...props }: LabelProps) {
     <>
       <label
         {...props}
-        className="block text-sm font-medium leading-6 text-gray-900 dark:text-gray-50 col-span-2"
+        className={classNames(
+          "block text-sm font-medium leading-6 text-gray-900 dark:text-gray-50 col-span-2",
+          props.className ?? "",
+        )}
       >
         {text}
       </label>
