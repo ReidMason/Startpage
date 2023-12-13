1501dc4a1bc64d582ac098371beebdd559904910
Reid Mason <maddogshain132@gmail.com>
2023-12-13 02:36:44 +0000

    Added config save button with save functionality

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
+import { classNames } from "@/utils/utils";
 import type { DetailedHTMLProps } from "react";
 
 interface ButtonProps
   extends DetailedHTMLProps<
-      React.ButtonHTMLAttributes<HTMLButtonElement>,
-      HTMLButtonElement
-    >,
-    React.AriaAttributes {}
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >,
+  React.AriaAttributes { }
 
-export const Button = (props: ButtonProps) => {
+export const Button = ({ className, ...props }: ButtonProps) => {
   return (
     <button
-      className="rounded-full text-primary-50 px-4 py-2 bg-primary-700"
+      className={classNames(
+        "rounded-full bg-primary-700 px-4 py-2 text-primary-50 dark:hover:bg-primary-600",
+        className ?? "",
+      )}
       {...props}
     >
       {props.children}
