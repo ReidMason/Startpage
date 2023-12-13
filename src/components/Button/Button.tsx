e8525c16d55ac5d3fc44d2c25cac6159bb140a91
Reid Mason <maddogshain132@gmail.com>
2023-12-13 02:51:20 +0000

    Added loading states for config section

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,27 @@
 import { classNames } from "@/utils/utils";
 import type { DetailedHTMLProps } from "react";
+import LoadingSpinner from "./LoadingSpinner";
 
 interface ButtonProps
   extends DetailedHTMLProps<
     React.ButtonHTMLAttributes<HTMLButtonElement>,
     HTMLButtonElement
   >,
-  React.AriaAttributes { }
+  React.AriaAttributes {
+  loading?: boolean;
+}
 
-export const Button = ({ className, ...props }: ButtonProps) => {
+export const Button = ({ className, loading, ...props }: ButtonProps) => {
   return (
     <button
       className={classNames(
-        "rounded-full bg-primary-700 px-4 py-2 text-primary-50 dark:hover:bg-primary-600",
+        "flex items-center justify-center rounded-full bg-primary-700 px-4 py-2 text-primary-50 disabled:cursor-not-allowed disabled:opacity-50 enabled:dark:hover:bg-primary-600",
         className ?? "",
       )}
+      disabled={loading}
       {...props}
     >
+      {loading && <LoadingSpinner />}
       {props.children}
     </button>
   );
