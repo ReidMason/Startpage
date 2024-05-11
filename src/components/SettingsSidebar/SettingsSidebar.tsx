8469fee2518de5cffd8e31e1c7e33e3ea9696c75
Reid Mason <maddogshain132@gmail.com>
2024-05-11 23:30:08 +0100

    refactor: created generic sidebar component

--- src/components/SettingsSidebar/SettingsSidebar.tsx
+++ src/components/SettingsSidebar/SettingsSidebar.tsx
@@ -1,6 +1,4 @@
-import { Fragment, HTMLAttributes, useEffect, useState } from "react";
-import { Dialog, Transition } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/24/outline";
+import { HTMLAttributes, useEffect, useState } from "react";
 import { Page as Page } from "./types";
 import { Config, configSchema } from "@/services/config/schemas";
 import { StateSetter } from "@/utils/common";
@@ -11,6 +9,7 @@ import { Button } from "../Button/Button";
 import { saveConfig } from "@/services/config/config";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form } from "@/components/ui/form";
+import Sidebar from "../Sidebar/Sidebar";
 
 interface SettingsSidebarProps extends HTMLAttributes<HTMLDivElement> {
   open: boolean;
@@ -68,7 +67,7 @@ export default function SettingsSidebar({
 
   return (
     <div>
-      <SidebarSlider open={open} setOpen={setOpen}>
+      <Sidebar open={open} setOpen={setOpen}>
         <Form {...form}>
           <form
             onSubmit={form.handleSubmit(onSubmit)}
@@ -94,7 +93,7 @@ export default function SettingsSidebar({
             </div>
           </form>
         </Form>
-      </SidebarSlider>
+      </Sidebar>
       <main
         className={classNames(
           open ? "md:ml-80 md:px-4" : "",
@@ -104,61 +103,5 @@ export default function SettingsSidebar({
         {children}
       </main>
     </div>
-  );
-}
-
-interface SidebarSliderProps {
-  open: boolean;
-  setOpen: (open: boolean) => void;
-  children: React.ReactNode;
-}
-
-function SidebarSlider({ open, setOpen, children }: SidebarSliderProps) {
-  return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-50" onClose={setOpen}>
-        <div className="fixed inset-0 flex">
-          <Transition.Child
-            as={Fragment}
-            enter="transition ease-in-out duration-300 transform"
-            enterFrom="-translate-x-full"
-            enterTo="translate-x-0"
-            leave="transition ease-in-out duration-300 transform"
-            leaveFrom="translate-x-0"
-            leaveTo="-translate-x-full"
-          >
-            <Dialog.Panel className="relative mr-16 flex w-full max-w-xs flex-1">
-              <Transition.Child
-                as={Fragment}
-                enter="ease-in-out duration-300"
-                enterFrom="opacity-0"
-                enterTo="opacity-100"
-                leave="ease-in-out duration-300"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-              >
-                <div className="absolute left-full top-0 flex w-16 justify-center pt-5">
-                  <button
-                    type="button"
-                    className="-m-2.5 p-2.5"
-                    onClick={() => setOpen(false)}
-                  >
-                    <span className="sr-only">Close sidebar</span>
-                    <XMarkIcon
-                      className="h-6 w-6 text-white"
-                      aria-hidden="true"
-                    />
-                  </button>
-                </div>
-              </Transition.Child>
-
-              <div className="grow bg-card px-3 pb-2 ring-1 ring-white/10">
-                {children}
-              </div>
-            </Dialog.Panel>
-          </Transition.Child>
-        </div>
-      </Dialog>
-    </Transition.Root>
   );
 }
