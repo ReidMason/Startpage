6dd5390666c5909f427e6a228f62bebf3bcf2cbd
Reid Mason <maddogshain132@gmail.com>
2024-05-12 03:40:29 +0100

    fix: fixed type errors on appearance settings tab

--- src/components/SettingsSidebar/Appearance.tsx
+++ src/components/SettingsSidebar/Appearance.tsx
@@ -2,9 +2,14 @@ import React from "react";
 import { SettingsSectionProps } from "./types";
 import SettingsPanelWrapper from "./SettingsPanelWrapper";
 
-export default function Appearance({ ...props }: SettingsSectionProps) {
+export default function Appearance({
+  setActivePage,
+  page,
+  control,
+  loading,
+}: SettingsSectionProps) {
   return (
-    <SettingsPanelWrapper {...props}>
+    <SettingsPanelWrapper setActivePage={setActivePage} panel={page}>
       <p className="text-white">Nothing here yet :(</p>
     </SettingsPanelWrapper>
   );
